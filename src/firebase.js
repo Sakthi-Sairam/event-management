6612757a786926ca6346badf6709760f38641b38
Sakthi-Sairam <sai74181@outlook.com>
2024-06-26 10:35:38 +0530

    dotenv added to safely handle api key

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,18 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { config } from 'dotenv';
+
+// Initialize dotenv to read .env file
+config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzTZNdlSX-K5MySrm1EOSTwei2pOcYU_c",
-  authDomain: "event-management-page.firebaseapp.com",
-  projectId: "event-management-page",
-  storageBucket: "event-management-page.appspot.com",
-  messagingSenderId: "409078410094",
-  appId: "1:409078410094:web:18145244abc0bcad47af7f"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
