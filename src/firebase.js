00b8e07a71aa922087ef4e46bdea27d5fdbc54f9
Sakthi-Sairam <sai74181@outlook.com>
2024-06-30 19:20:50 +0530

    firebase tip

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
+const firebaseConfig = { //add your own firebase configuration
   apiKey: "",
   authDomain: "",
   projectId: "",
